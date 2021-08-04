2e26df431733fc7a9ac5f542c6576d7d68e42255
DevilRaider-Dev <deviltwix3@gmail.com>
2021-08-04 15:42:41 +0200

    add mediaquery resolution between lower and higher 768px

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,12 @@
 Font: Karla
 Farbe: #323A45 / #848484
 */
+
+function collapse() {
+    var x = document.getElementById("navLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
